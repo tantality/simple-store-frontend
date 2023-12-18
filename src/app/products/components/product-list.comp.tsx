b793b0ba17de8bf717c5cba13924bf919a7abdf0
tantality <angel.tretyak@mail.ru>
2023-12-19 00:34:55 +0300

    feat: implement receiving all products from a server

--- src/app/products/components/product-list.comp.tsx
+++ src/app/products/components/product-list.comp.tsx
@@ -1,47 +1,21 @@
-import { FC } from "react";
-import { ProductDto } from "../types/product.dto";
+import CenteredLoader from "components/centered-loader.comp";
+import { useAppDispatch, useAppSelector } from "hooks/redux.hooks";
+import { FC, useEffect } from "react";
+import { getAllProducts } from "../store/products.actions";
+import { productsSelector } from "../store/products.selectors";
 import ProductCards from "./product-cards.comp";
 
 const ProductList: FC = () => {
-  const products: ProductDto[] = [
-    {
-      id: '2',
-      name: 'Chevrolet ORLANDO',
-      img: "https://avcdn.av.by/wisiwigimage/0001/8199/9306.jpeg",
-      price: 2424,
-      createdAt: 222,
-      updatedAt: 323,
-      quantity: 3,
-    },
-    {
-      id: '3',
-      name: 'Chrysler Grand Voyager',
-      img: 'https://www.ixbt.com/img/x780/n1/news/2022/9/1/shutterstock_2038483553_large.png',
-      price: 289,
-      createdAt: 222,
-      updatedAt: 323,
-      quantity: 3,
-    },
-    {
-      id: '4',
-      name: 'Dodge Grand Caravan ',
-      img: 'https://s.mediasalt.ru/cache/content/data/images/339/339855/original.jpg',
-      price: 893,
-      createdAt: 222,
-      updatedAt: 323,
-      quantity: 3,
-    },
-    {
-      id: '5',
-      name: 'FORD Focus 2',
-      img: 'https://5koleso.ru/wp-content/uploads/2022/01/img_5769-1024x683.jpg',
-      price: 123,
-      createdAt: 222,
-      updatedAt: 323,
-      quantity: 3,
-    }
-  ];
+  const dispatch = useAppDispatch();
+  const { isPending, products } = useAppSelector(productsSelector);
 
+  useEffect(() => {
+    dispatch(getAllProducts({}))
+  }, [dispatch])
+
+  if (isPending) {
+    <CenteredLoader />
+  }
 
   return (
     <>
