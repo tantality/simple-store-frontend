480675f3c773174787a75b85157e799cf305c48c
tantality <angel.tretyak@mail.ru>
2023-12-19 01:17:14 +0300

    feat: add product pagination

--- src/app/products/components/product-list.comp.tsx
+++ src/app/products/components/product-list.comp.tsx
@@ -1,17 +1,30 @@
+import { Pagination } from "@mui/material";
 import CenteredLoader from "components/centered-loader.comp";
 import { useAppDispatch, useAppSelector } from "hooks/redux.hooks";
-import { FC, useEffect } from "react";
+import { ChangeEvent, FC, useEffect, useState } from "react";
 import { getAllProducts } from "../store/products.actions";
 import { productsSelector } from "../store/products.selectors";
 import ProductCards from "./product-cards.comp";
 
+
 const ProductList: FC = () => {
+  const [currentPage, setCurrentPage] = useState<number>(1);
+  const [pageCount, setPageCount] = useState<number | null>(null);
+
   const dispatch = useAppDispatch();
   const { isPending, products } = useAppSelector(productsSelector);
 
+  const handleCurrentPageChange = (event: ChangeEvent<unknown>, page: number): void => {
+    setCurrentPage(page);
+  };
+
   useEffect(() => {
-    dispatch(getAllProducts({}))
-  }, [dispatch])
+    dispatch(getAllProducts({
+      query: {
+        pageNumber: currentPage
+      }
+    }))
+  }, [dispatch, currentPage])
 
   if (isPending) {
     <CenteredLoader />
@@ -20,6 +33,7 @@ const ProductList: FC = () => {
   return (
     <>
       <ProductCards products={products} />
+      {pageCount && <Pagination onChange={handleCurrentPageChange} page={currentPage} count={pageCount} size="large" />}
     </>
   );
 }
