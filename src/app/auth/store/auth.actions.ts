ff9bba201c6d718628985659e0d4947f583669e6
tantality <angel.tretyak@mail.ru>
2023-12-19 22:29:24 +0300

    fix: change the auth actions

--- src/app/auth/store/auth.actions.ts
+++ src/app/auth/store/auth.actions.ts
@@ -6,7 +6,7 @@ import { SignInForm } from "../types/signin.form";
 
 export const signUp = createAsyncThunk<AuthDto, { body: SignUpForm }>("POST/signup", async ({ body }, { rejectWithValue }) => {
   try {
-    const response = await axiosClient.post<AuthDto>("/auth/signup", body);
+    const response = await axiosAuthClient.post<AuthDto>("/auth/signup", body);
     localStorage.setItem("access-token", response.data.accessToken);
     return response.data;
   } catch (error: any) {
@@ -17,7 +17,7 @@ export const signUp = createAsyncThunk<AuthDto, { body: SignUpForm }>("POST/sign
 
 export const signIn = createAsyncThunk<AuthDto, { body: SignInForm }>("POST/signin", async ({ body }, { rejectWithValue }) => {
   try {
-    const response = await axiosClient.post<AuthDto>("/auth/signin", body);
+    const response = await axiosAuthClient.post<AuthDto>("/auth/signin", body);
     localStorage.setItem("access-token", response.data.accessToken);
     return response.data;
   } catch (error: any) {
@@ -38,7 +38,7 @@ export const signOut = createAsyncThunk<AuthDto>("GET/signout", async (_, { reje
 
 export const refreshTokens = createAsyncThunk<AuthDto>("POST/refresh-tokens", async (_, { rejectWithValue }) => {
   try {
-    const response = await axiosClient.post("/auth/signout", _, { withCredentials: true });
+    const response = await axiosAuthClient.post("/auth/refresh-tokens");
     localStorage.setItem("access-token", response.data.accessToken);
     return response.data;
   } catch (error: any) {
