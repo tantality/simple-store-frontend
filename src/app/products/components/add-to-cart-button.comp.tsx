0cd000ff161920ed1e6674f697823e95f295e95d
tantality <angel.tretyak@mail.ru>
2023-12-20 21:25:08 +0300

    feat: implement an add-to-cart bnutton

--- src/app/products/components/add-to-cart-button.comp.tsx
+++ src/app/products/components/add-to-cart-button.comp.tsx
@@ -3,43 +3,11 @@ import { FC } from "react";
 
 interface AddToCartButtonProps {
   isProductInCart: boolean;
-  // product
 }
 
-//product
-// 
-
 const AddToCartButton: FC<AddToCartButtonProps> = ({ isProductInCart }) => {
-  // const
   const btnText = isProductInCart ? 'In cart' : 'Add to card';
-  const activeBtnStyles = { fullWidth: true, variant: 'contained' };
-  const disabledBtnStyles = {};
-
-  const btnStyles = isProductInCart ? disabledBtnStyles : activeBtnStyles;
-
-
-  return (
-    <>
-      <Button disabled fullWidth={true} variant="contained" onClick={() => console.log('fef')}>{btnText}</Button>
-    </>
-  )
-  // createOrder / addOrderItem
+  return <Button fullWidth={true} variant="contained" disabled={isProductInCart}>{btnText}</Button>
 }
 
 export default AddToCartButton;
-
-// addOrderItem  | post('orderId/items')
-
-// param
-// orderId
-
-
-// body
-// productId
-// quantity
-
-
-// createOrder
-
-// body
-// item : productId, quantity
