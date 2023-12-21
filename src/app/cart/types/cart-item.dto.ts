d97f47fe691ae94404dbb541ac820af334731f43
tantality <angel.tretyak@mail.ru>
2023-12-21 04:04:51 +0300

    refactor: delete an extra type

--- src/app/cart/types/cart-item.dto.ts
+++ src/app/cart/types/cart-item.dto.ts
@@ -1,11 +1,5 @@
+import { ProductDto } from "app/products/types/product.dto";
 import { UUIDDto } from "types/uuid-dto.type";
-
-interface ProductDto extends UUIDDto {
-  name: string;
-  quantity: number;
-  price: number;
-  img: string;
-}
 
 export interface CartItemDto extends UUIDDto {
   orderId: string;
