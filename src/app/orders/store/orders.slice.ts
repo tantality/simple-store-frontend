f076222965f7f7cf5a999420e33b6f7cc03cf51e
tantality <angel.tretyak@mail.ru>
2023-12-23 03:29:54 +0300

    feat: change the orders slice

--- src/app/orders/store/orders.slice.ts
+++ src/app/orders/store/orders.slice.ts
@@ -3,11 +3,14 @@ import { OrdersState } from "../types/orders.state";
 import { getUserOrders } from "./orders.actions";
 
 const initialState: OrdersState = {
+  count: null,
   orders: [],
   isPending: {
+    count: false,
     orders: false,
   },
   errors: {
+    count: null,
     orders: null,
   },
 };
@@ -20,15 +23,21 @@ export const ordersSlice = createSlice({
     builder
       .addCase(getUserOrders.pending, (state) => {
         state.isPending.orders = true;
+        state.isPending.count = true;
         state.errors.orders = null;
+        state.errors.count = null;
       })
       .addCase(getUserOrders.fulfilled, (state, { payload }) => {
         state.isPending.orders = false;
-        state.orders = payload;
+        state.isPending.count = false;
+        state.orders = payload.orders;
+        state.count = payload.count;
       })
       .addCase(getUserOrders.rejected, (state, action: any & { payload: any }) => {
         state.isPending.orders = false;
+        state.isPending.count = false;
         state.errors.orders = action.payload.message;
+        state.errors.count = action.payload.message;
       });
   },
 });
